acbe0405ff085678393d6a0c636149e65fb68cda
jnxng <s10222060@connect.np.edu.sg>
2024-01-04 18:06:05 +0800

    initial test cases

---
+++ testing_scripts/test_register.py
@@ -0,0 +1,10 @@
+
+def test_register_successfully(client):
+    response = client.post('/register', json={'username': 'newUser', 'password': 'newPassword'})
+    assert response.status_code == 200
+    assert b'Registration successful' in response.data
+
+def test_existing_registry(client):
+    response = client.post('/register', json={'username': 'correctName', 'password': 'newPassword'})
+    assert response.status_code == 400
+    assert b'Username already exists. Please choose another username.' in response.data
