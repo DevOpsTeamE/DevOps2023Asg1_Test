2d6ac19b187017b3ac50b661852806d321866741
jnxng <s10222060@connect.np.edu.sg>
2024-01-06 14:55:48 +0800

    fixing initial test cases, adding fixtures

--- test_login.py
+++ test_login.py
@@ -1,3 +1,17 @@
+import pytest
+from main import create_app 
+
+@pytest.fixture
+def app():
+    app = create_app()
+    app.config.update({"TESTING": True})
+
+    with app.test_client() as client:
+        yield client
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
 
 
 def test_login_successfully(client):
