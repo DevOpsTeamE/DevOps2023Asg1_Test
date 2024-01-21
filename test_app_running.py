eeef0fc370c8a81601b1d8311e61fc42942f4cbe
ChunKaii <s10222222@connect.np.edu.sg>
2024-01-21 09:30:50 +0800

    Modified test app running

--- test_app_running.py
+++ test_app_running.py
@@ -1,5 +1,8 @@
 import pytest
-from main import create_app 
+from main import create_app
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys 
 
 @pytest.fixture
 def app():
@@ -15,7 +18,13 @@ def client(app):
 
 def test_starting_app(client):
     response = client.get('/')
-    assert response.status_code == 200
-    assert b'Welcome to My Website' in response.data
-    assert b'<button>Login</button>' in response.data
-    assert b'<button>Register</button>' in response.data
+    button_text = "Login"
+    loginButton = response.find_element(By.XPATH, f'//button[text()="{button_text}"]') #Find button with text containing "Login"
+    loginButton.click() #Click Login Button
+    response.implicitly_wait(5) #Wait for 5 seconds
+    current_page = response.current_url.split("/")[-1] #Get the name of the html file that user is redirected to after clicking the login button
+    assert current_page == "login.html" 
+    #assert response.status_code == 200
+    #assert b'Welcome to My Website' in response.data
+    #assert b'<button>Login</button>' in response.data
+    #assert b'<button>Register</button>' in response.data
