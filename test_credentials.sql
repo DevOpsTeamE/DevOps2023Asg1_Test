12f30580330f5f55e1c26e75b7ba275c801b299d
joshuang321 <joshuang21978@gmail.com>
2024-01-27 16:49:11 +0800

    Fixed sql file

--- test_credentials.sql
+++ test_credentials.sql
@@ -1,5 +1,3 @@
 USE DevOps2023Asg1;
-
 INSERT INTO user VALUES('test_user', '', 0, 1);
-
-INSERT INTO capstone VALUES(('test_capstone_title', 'test_capstone_pic'));
+INSERT INTO capstone VALUES('test_capstone_title', 'test_capstone_pic');
