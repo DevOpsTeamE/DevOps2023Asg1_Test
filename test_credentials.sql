0c5fdb11ce0bc68a27237c46e30ed16b2c560fd7
joshuang321 <joshuang21978@gmail.com>
2024-01-27 16:54:40 +0800

    Added removed test creds from dev

--- test_credentials.sql
+++ test_credentials.sql
@@ -1,4 +1,6 @@
 USE DevOps2023Asg1;
+
 INSERT INTO user VALUES('joshuang321', 'password123!', 0, 1);
+INSERT INTO user VALUES('joshuang_user321', 'password123!', 1, 1);
 INSERT INTO user VALUES('test_user', '', 0, 1);
 INSERT INTO capstone VALUES('test_capstone_pic', 0, 1, 2024, 'test_capstone_title', 'test_capstone_companyname', 'test_capstone_poc', 'test_capstone_description');
