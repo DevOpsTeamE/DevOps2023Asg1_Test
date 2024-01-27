bd84809607751ff0382545b8b27624afe2fbf0e9
joshuang321 <joshuang21978@gmail.com>
2024-01-27 16:52:02 +0800

    Fixed file again lol

--- test_credentials.sql
+++ test_credentials.sql
@@ -1,4 +1,4 @@
-USE DATABASE DevOps2023Asg1;
+USE DevOps2023Asg1;
 INSERT INTO user VALUES('joshuang321', 'password123!', 0, 1);
 INSERT INTO user VALUES('test_user', '', 0, 1);
 INSERT INTO capstone VALUES('test_capstone_pic', 0, 1, 2024, 'test_capstone_title', 'test_capstone_companyname', 'test_capstone_poc', 'test_capstone_description');
