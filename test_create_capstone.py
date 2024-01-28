bf73dff835766c6f487157b21df3b3fd2d079ad6
ChunKaii <s10222222@connect.np.edu.sg>
2024-01-28 16:47:02 +0800

    Modified test case parameters

--- test_create_capstone.py
+++ test_create_capstone.py
@@ -3,11 +3,11 @@ from controllers.utilities.capstone import create_capstone, query_capstone
 
 
 def test_create_capstone_successfully(client):
-    create_capstone('test_name_1', 'student', '4', '2024', 'test_title', 'test_company', 'test_contact', 'test_description')
+    create_capstone('test_name_1', '1', '4', '2024', 'test_title', 'test_company', 'test_contact', 'test_description')
     capstones = query_capstone('2024', 'test_title')
     capstone =capstones[0]
     assert capstone.pic =='test_name_1'
-    assert capstone.role_id =='student'
+    assert capstone.role_id =='1'
     assert capstone.nstudent =='4'
     assert capstone.year =='2024'
     assert capstone.title =='test_title'
